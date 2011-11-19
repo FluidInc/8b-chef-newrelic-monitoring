4c205ee347817c740ed44242cdf165a20d2fc811
Nathen Harvey <nharvey@customink.com>
2011-11-19 07:24:16 -0500

    remove the old pid file, too

--- recipes/default.rb
+++ recipes/default.rb
@@ -66,15 +66,15 @@ end
 execute "kill old new relic server agent" do
   # This is a clean-up task that will handle a newrelic-sysmond
   # with its pid file in a non-standard location
-  command "kill `cat /var/run/nrsysmond.pid`"
+  command "kill `cat /var/run/nrsysmond.pid` && rm /var/run/nrsysmond.pid"
   action :nothing
   only_if do File.exist?("/var/run/nrsysmond.pid") end
 end
 
 directory "/var/run/newrelic" do
   recursive true
-  owner "root"
-  group "root"
+  owner "newrelic"
+  group "newrelic"
   notifies :run, "execute[kill old new relic server agent]", :immediately
 end
 
